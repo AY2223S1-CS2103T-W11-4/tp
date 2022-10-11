2fb95c730edaab5eda4e5afdd63f2a7009d7ced2
pewggls <97406702+pewggls@users.noreply.github.com>
2022-10-11 16:32:46 +0800

    Revert "Update itemlist"

--- src/main/java/seedu/waddle/model/itinerary/Item.java
+++ src/main/java/seedu/waddle/model/itinerary/Item.java
@@ -18,23 +18,4 @@ public class Item {
         requireNonNull(description);
         this.description = description;
     }
-
-    public String getDescription() {
-        return description;
-    }
-
-    /**
-     * Returns true if both items have the same description.
-     * This defines a weaker notion of equality between two persons.
-     */
-    public boolean isSameItem(Item otherItem) {
-        if (otherItem == this) {
-            return true;
-        }
-
-        return otherItem != null
-                && otherItem.getDescription().equals(getDescription());
-    }
-
-
 }
--- src/main/java/seedu/waddle/model/itinerary/exceptions/ItemNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package seedu.waddle.model.itinerary.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified item.
- */
-public class ItemNotFoundException extends RuntimeException {
-}
